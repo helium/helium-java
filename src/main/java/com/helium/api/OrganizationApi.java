3e2072de229fe17c20aa678972c2832ebc4e0ce2
Lane Seppala <lane@helium.com>
2017-03-11 15:01:38 -0700

    Add update to Organization

--- src/main/java/com/helium/api/OrganizationApi.java
+++ src/main/java/com/helium/api/OrganizationApi.java
@@ -1,12 +1,25 @@
 package com.helium.api;
 
 import com.github.jasminb.jsonapi.JSONAPIDocument;
+import com.helium.model.Metadata;
 import com.helium.model.Organization;
 import retrofit2.Call;
-import retrofit2.http.GET;
+import retrofit2.http.*;
 
 public interface OrganizationApi {
 
     @GET("organization")
     Call<JSONAPIDocument<Organization>> organization();
+
+    @PATCH("organization")
+    Call<JSONAPIDocument<Organization>> updateOrganization(@Body Organization organization);
+
+    @GET("organization/metadata")
+    Call<Metadata> organizationMetadata();
+
+    @PATCH("organization/metadata")
+    Call<Metadata> updateOrganizationMetadata(@Body Metadata metadata);
+
+    @PUT("organization/metadata")
+    Call<Metadata> replaceOrganizationMetadata(@Body Metadata metadata);
 }
--- src/main/java/com/helium/client/Organization.java
+++ src/main/java/com/helium/client/Organization.java
@@ -1,10 +1,11 @@
 package com.helium.client;
 
 import com.helium.api.HeliumApi;
+import com.helium.model.Metadata;
 
 import java.io.IOException;
 
-public class Organization {
+public class Organization implements HasMetadata {
 
     private HeliumApi api;
     private com.helium.model.Organization model;
@@ -18,4 +19,41 @@ public class Organization {
     public static Organization organization(HeliumApi api) throws IOException {
         return new Organization(api, api.org.organization().execute().body().get());
     }
+
+    public Organization setName(String name) throws IOException {
+        return setAttributes(name, model.getTimezone());
+    }
+
+    public Organization setTimezone(String timezone) throws IOException {
+        return setAttributes(model.getName(), timezone);
+    }
+
+    public Organization setAttributes(String name, String timezone) throws IOException {
+        model.setName(name);
+        model.setTimezone(timezone);
+        return new Organization(api, api.org.updateOrganization(model).execute().body().get());
+    }
+
+    @Override
+    public Metadata metadata() throws IOException {
+        return api.org.organizationMetadata().execute().body();
+    }
+
+    @Override
+    public Organization updateMetadata(Metadata metadata) throws IOException {
+        api.org.updateOrganizationMetadata(metadata).execute().body();
+        return this;
+    }
+
+    @Override
+    public Organization replaceMetadata(Metadata metadata) throws IOException {
+        api.org.replaceOrganizationMetadata(metadata).execute().body();
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return model.toString();
+    }
+
 }
